e476c95c09f598b98fc326c6f5d04252c797ab7a
rabeeahc <122523423+rabeeahc@users.noreply.github.com>
2025-10-04 01:38:05 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
+import './globals.css';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
