4c1c27454d061b365790945d74aa651e25679c94
rabeeahc <122523423+rabeeahc@users.noreply.github.com>
2025-10-04 01:39:20 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Empaerial. All rights reserved.</p>
       <div className={styles.social}>
-        <a href="#">Twitter</a>
-        <a href="#">LinkedIn</a>
+        <a
+          href="https://www.instagram.com/_empaerial_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/emp%C3%A6rial/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
       </div>
+
+      <p>© 2025 Empaerial. All rights reserved.</p>
     </footer>
   );
 };
