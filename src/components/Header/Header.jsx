52b72943f34dd7b9b1a093f00b06e1bf59c95363
rabeeahc <122523423+rabeeahc@users.noreply.github.com>
2025-10-04 01:39:54 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,21 @@
+"use client";
 import React from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Empaerial</div>
       <nav className={styles.nav}>
-        <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
       </nav>
-      <div className={styles.hamburger}>☰</div>
+
+      <div className={styles.logo}>EMPÆRIAL</div>
+
+      {}
+      
     </header>
   );
 };
