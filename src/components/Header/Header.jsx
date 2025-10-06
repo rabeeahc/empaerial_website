29d08559603270b9454cb386ca5d36b8a0eb76ba
Rabeeahc <rabeeahc2003@gmail.com>
2025-10-06 18:54:09 +0300

    Hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+'use client';
+
+import React, { useState } from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <span className={styles.keyword}>CONST</span>{" "}
-        <span className={styles.variable}>EMPÆRIAL</span>{" "}
-        <span className={styles.function}>=</span>{" "}
-        <span className={styles.string}>"UAV Team"</span>
+        <span className={styles.keyword}>EM</span>
+        <span className={styles.function}>P</span>
+        <span className={styles.number}>Æ</span>
+        <span className={styles.function}>RI</span>
+        <span className={styles.number}>AL</span>
       </div>
-      <nav className={styles.nav}>
+
+      <nav className={`${styles.nav} ${isOpen ? styles.navOpen : ""}`}>
         <a href="#hero" className={styles.function}>Home()</a>
         <a href="#about" className={styles.keyword}>About()</a>
         <a href="#project" className={styles.variable}>Project()</a>
         <a href="#contact" className={styles.string}>Contact()</a>
       </nav>
+
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   );
 };
